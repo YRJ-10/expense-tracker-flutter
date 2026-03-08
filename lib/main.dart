99fb4ba800f716e8781f2461ea29a56465dd2de7
YRJ <yeryindra@gmail.com>
2026-03-08 18:24:47 +0700

    Replace Supabase credentials with placeholders
    Updated Supabase initialization with placeholders for URL and anon key.

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://niuboxpvuazwgazdkobw.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5pdWJveHB2dWF6d2dhemRrb2J3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NTEwMTksImV4cCI6MjA4ODUyNzAxOX0.LnUCRaSAF5BW-lJcdE9dRcMKNanbV0RO3lyartySvA8',
+  url: 'YOUR_SUPABASE_URL',
+  anonKey: 'YOUR_SUPABASE_ANON_KEY',
     authOptions: const FlutterAuthClientOptions(
       authFlowType: AuthFlowType.pkce,
     ),
